8c1654f2d7067c0740fb9464bf41ee4fa9912d03
Okpara202 <okparafavour202@gmail.com>
2025-03-20 15:15:59 +0100

    Fixed ts errors preventing deployment

--- src/components/breadCrumb.tsx
+++
@@ -1,9 +0,0 @@
-import { Link, useLocation } from "react-router-dom";
-function BreadCrumb() {
-  const location = useLocation();
-  const path = location.pathname;
-
-  return <></>;
-}
-
-export default BreadCrumb;
